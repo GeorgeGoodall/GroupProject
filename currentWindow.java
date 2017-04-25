63131556fb0fa7f29761cccf9a55356399cf486d
evdokiamina <evdokia_mina@hotmail.com>
2017-04-25 14:58:42 +0100

    Add files via upload

--- currentWindow.java
+++ currentWindow.java
@@ -9,7 +9,7 @@ public class currentWindow extends Application{
 	@Override
 	public void start(Stage stage) {
 		try{
-			Parent root = FXMLLoader.load(getClass().getResource("/loginScreenBuilder.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/OpeningScreen.fxml"));
 			Scene scene = new Scene(root);
 	        stage.setScene(scene);
 	        stage.setTitle("Login");
